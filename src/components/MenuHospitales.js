f106948b553930f1a999c7489bbabd128c5dc634
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-21 09:57:30 +0200

    inicializando app

---
+++ src/components/MenuHospitales.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class MenuHospitales extends Component {
+    render() {
+        return (<div>
+            <h1>Menu de hospitales</h1>
+        </div>)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
+import MenuHospitales from './components/MenuHospitales';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<div>
+  <Home />
+  <MenuHospitales />
+</div>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
